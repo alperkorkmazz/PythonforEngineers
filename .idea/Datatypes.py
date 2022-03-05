885559fc97637b66a899bd1dbb44c12ad2d18d08
_Nomad_ <62549015+dralperkorkmaz@users.noreply.github.com>
2022-03-05 20:46:28 +0100

    First

---
+++ .idea/Datatypes.py
@@ -0,0 +1,8 @@
+"""
+Find the Ascii numbers of each character in the expression 'Hello World. I am 07.'
+ord(x) function finds the ascii value of the given character paramater
+"""
+
+word='Hello World. I Am 07. Extra-ü-ö-ä'
+for x in word:
+    print("Letter: ",x, " Ascii: ", ord(x))
