984381330574bfa496977e479a7acb363d692ec2
_Nomad_ <62549015+dralperkorkmaz@users.noreply.github.com>
2022-03-05 21:11:50 +0100

    First

---
+++ .idea/simpleCryption.py
@@ -0,0 +1,15 @@
+"""
+crypt the sentence 'Hi Friend. How are you 07? I am fine.' using the key 8
+write the crypted and decrypted forms on screen
+"""
+message='Hi Friend. How are you 07? I am fine.'
+key=8
+encrypted=""
+for x in message:
+    encrypted=encrypted+chr(ord(x)+8)
+print(encrypted)
+decrypted=""
+for x in encrypted:
+    decrypted=decrypted+chr(ord(x)-8)
+
+print(decrypted)
